6993acd2a4583579d2444d718653f447fd536b09
Gilang Satria <satria.prayoga@gmail.com>
2018-10-03 00:16:44 +0700

    First commit

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 
+import 'bootstrap';
+import '@fortawesome/fontawesome-free';
+
 Vue.config.productionTip = false
 
 new Vue({
